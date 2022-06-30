b7b562789b807e2a7d403b729099aa3f614fc8f1
AkumuYuma <manumanu1355@gmail.com>
2022-06-30 20:07:46 +0200

    added template project made with spring initializr

---
+++ src/test/java/com/springSecurityDemo/securityDemo/SecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springSecurityDemo.securityDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
